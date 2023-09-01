2761d439ff4a5b6e8781a5f7c33654de56c26d27
= <gemechis.elias@a2sv.org>
2023-09-01 13:30:03 +0300

    Exercise API Added

--- app/Repositories/ExerciseRepository.php
+++ app/Repositories/ExerciseRepository.php
@@ -80,8 +80,10 @@ class ExerciseRepository implements CrudInterface
     public function create(array $data): Exercise
     {
         $titleShort      = Str::slug(substr($data['question'], 0, 20));
-        $data['user_id'] = "1";
+        if (empty($data['user_id'])) {
+        $data['user_id'] = $this->user->id;
 
+            }
         if (!empty($data['image'])) {
             $data['image'] = UploadHelper::upload('image', $data['image'], $titleShort . '-' . time(), 'images/exercises');
         }
