1270f0616b4922288b243c926d2405341ce33781
= <gemechis.elias@a2sv.org>
2023-09-13 13:01:58 +0300

    Progress API Added

--- database/seeders/UnitSeeder.php
+++ database/seeders/UnitSeeder.php
@@ -27,9 +27,104 @@ class UnitSeeder extends Seeder
                 'unit_title' => 'Meetings and Greetings',
                 'unit_description' => 'Learn the basic greetings you\'ll need to introduce yourself and have your first conversation, plus essential pronunciation of -ch, -h, -ll, and -ñ.',
                 'image' => "https://api.globaltongueedu.com/v1/public/images/units/image_1.jpg",
+                
+                
 
             ],
+            [
+                'course_id'=> 1,
+                'level_id' => 1,
+                'user_id' => 1,
 
+                'unit_name' => 'Unit 2', 
+                'unit_title' => 'Let\'s Talk About You',
+                'unit_description' => 'Explore subject pronouns, professions, and using "ser" for descriptions and origins. Learn pronunciation of vowels and when to use "tú" versus "usted."',
+                'image' => "https://api.globaltongueedu.com/v1/public/images/units/image_1.jpg",
+            
+                
+
+            ],
+            [
+                'course_id'=> 1,
+                'level_id' => 1,
+                'user_id' => 1,
+                'unit_name' => 'Unit 3', 
+                'unit_title' => 'Let\'s Talk About You',
+                'unit_description' => 'Explore subject pronouns, professions, and using "ser" for descriptions and origins. Learn pronunciation of vowels and when to use "tú" versus "usted."',
+                'image' => "https://api.globaltongueedu.com/v1/public/images/units/image_1.jpg",
+                
+                 
+
+            ],
+            [
+                'course_id'=> 1,
+                'level_id' => 2,
+                'user_id' => 1,
+                'unit_name' => 'Unit 1', 
+                'unit_title' => 'Let\'s Talk About You',
+                'unit_description' => 'Explore subject pronouns, professions, and using "ser" for descriptions and origins. Learn pronunciation of vowels and when to use "tú" versus "usted."',
+                'image' => "https://api.globaltongueedu.com/v1/public/images/units/image_1.jpg",
+                
+                 
+
+            ],
+            [
+                'course_id'=> 1,
+                'level_id' => 2,
+                'user_id' => 1,
+                'unit_name' => 'Unit 2', 
+                'unit_title' => 'Let\'s Talk About You',
+                'unit_description' => 'Explore subject pronouns, professions, and using "ser" for descriptions and origins. Learn pronunciation of vowels and when to use "tú" versus "usted."',
+                'image' => "https://api.globaltongueedu.com/v1/public/images/units/image_1.jpg",
+                
+                 
+
+            ],
+            [
+                'course_id'=> 2,
+                'level_id' => 1,
+                'user_id' => 1,
+                'unit_name' => 'Unit 1', 
+                'unit_title' => 'Let\'s Talk About You',
+                'unit_description' => 'Explore subject pronouns, professions, and using "ser" for descriptions and origins. Learn pronunciation of vowels and when to use "tú" versus "usted."',
+                'image' => "https://api.globaltongueedu.com/v1/public/images/units/image_1.jpg",
+                
+                 
+
+            ],
+            [
+                'course_id'=> 2,
+                'level_id' => 1,
+                'user_id' => 1,
+                'unit_name' => 'Unit 2', 
+                'unit_title' => 'Let\'s Talk About You',
+                'unit_description' => 'Explore subject pronouns, professions, and using "ser" for descriptions and origins. Learn pronunciation of vowels and when to use "tú" versus "usted."',
+                'image' => "https://api.globaltongueedu.com/v1/public/images/units/image_1.jpg",
+                
+            ],
+            [
+                'course_id'=> 2,
+                'level_id' => 2,
+                'user_id' => 1,
+                'unit_name' => 'Unit 1', 
+                'unit_title' => 'Let\'s Talk About You',
+                'unit_description' => 'Explore subject pronouns, professions, and using "ser" for descriptions and origins. Learn pronunciation of vowels and when to use "tú" versus "usted."',
+                'image' => "https://api.globaltongueedu.com/v1/public/images/units/image_1.jpg",
+        
+            ],
+            [
+                'course_id'=> 3,
+                'level_id' => 1,
+                'user_id' => 1,
+                'unit_name' => 'Unit 1', 
+                'unit_title' => 'Let\'s Talk About You',
+                'unit_description' => 'Explore subject pronouns, professions, and using "ser" for descriptions and origins. Learn pronunciation of vowels and when to use "tú" versus "usted."',
+                'image' => "https://api.globaltongueedu.com/v1/public/images/units/image_1.jpg",
+                
+                 
+
+            ],
+            
          
         ];
         Unit::insert($data);
